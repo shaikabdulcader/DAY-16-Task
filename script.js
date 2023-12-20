2b3bd7a63127aa5a74d59d9cc750a6b12523e243
Shaik Abdul Cader <119555898+shaikabdulcader@users.noreply.github.com>
2023-12-21 01:24:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+setTimeout(function name(params) {
+    const first = document.querySelector("#span-1");
+    first.textContent = "10 😶‍🌫️";
+    setTimeout(function name(params) {
+      const second = document.querySelector("#span-1");
+      second.textContent = "9 🤩";
+      setTimeout(function name(params) {
+        const third = document.querySelector("#span-1");
+        third.textContent = "8 😍";
+        setTimeout(function name(params) {
+          const fourth = document.querySelector("#span-1");
+          fourth.textContent = "7 🤩";
+          setTimeout(function name(params) {
+            const fifth = document.querySelector("#span-1");
+            fifth.textContent = "6 😍";
+            setTimeout(function name(params) {
+              const sixth = document.querySelector("#span-1");
+              sixth.textContent = "5 🤩";
+              setTimeout(function name(params) {
+                const seven = document.querySelector("#span-1");
+                seven.textContent = "4 😍";
+                setTimeout(function name(params) {
+                  const eight = document.querySelector("#span-1");
+                  eight.textContent = "3 🤩";
+                  setTimeout(function name(params) {
+                    const nine = document.querySelector("#span-1");
+                    nine.textContent = "2 😍";
+                    setTimeout(function name(params) {
+                      const ten = document.querySelector("#span-1");
+                      ten.textContent = "1 🥳";
+                      setTimeout(function name(params) {
+                        const ans = (document.querySelector("#span-1").innerText =
+                          "Happy Independance Day 🎉");
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+   },1000);
